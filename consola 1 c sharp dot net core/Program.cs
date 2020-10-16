60640639a5d866dc40c13672ee31d56e21cff24f
Krisztian Barata <iilexlevisii@gmail.com>
2020-10-16 16:14:40 +0300

    Am adaugat la  MyClass

---
+++ consola 1 c sharp dot net core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consola_1_c_sharp_dot_net_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
